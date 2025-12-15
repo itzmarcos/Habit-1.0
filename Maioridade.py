db6d6f64f5367ba345acab51498c91e417b78dc4
itzmarcos <marcozfuwa@gmail.com>
2025-12-15 16:13:27 -0300

    initial

---
+++ Maioridade.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+atual = datetime.now().year
+
+maior = 0
+menor = 0
+
+for c in range(1, 8):
+    nasc = int(input(f'Em que ano a {c} pessoa nasceu: '))
+    idade = atual - nasc
+    if idade >= 18:  
+        maior += 1
+    else:
+        menor += 1
+print(f'Ao todo tivemos {maior} pessoas maiores de idade.')
+print(f'E tivemos {menor} pessoas menores de idade')
+
---
+++ Peso.py
@@ -0,0 +1,15 @@
+maior = 0
+menor = 0
+for p in range(1, 5):
+    peso = float(input(f'O peso da {p} é: '))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f'O maior peso lido foi {maior}')
+print(f'O menor peeso lido foi {menor}')
