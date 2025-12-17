4462c24337f83c4464a802d209d81daa99127579
itzmarcos <marcozfuwa@gmail.com>
2025-12-17 13:22:09 -0300

    FP

---
+++ Fatorial.py
@@ -0,0 +1,4 @@
+from math import factorial
+n = int(input('Digite um numero para calcular seu Fatorial: '))
+f = factorial(n)
+print(f'O fatorial de {n} é {f}')
---
+++ P_Aritmetica.py
@@ -0,0 +1,9 @@
+p = int(input('Primeiro termo: '))
+r = int(input('Razão da PA: '))
+termo = p
+cont = 1
+while cont <= 10:
+    print(f'{termo} -> ', end='')
+    termo += r
+    cont += 1
+print('END')
